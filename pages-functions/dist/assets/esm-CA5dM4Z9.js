ad488a3260c9b928737eb26807ea3ec0cb7a8e5c
Yao <yao@example.com>
2026-09-03 12:40:43 +0800

    fix: include pages-functions/dist in git for Cloudflare Pages auto-build; remove duplicate AUTH_JWT_SECRET from wrangler.toml

---
+++ pages-functions/dist/assets/esm-CA5dM4Z9.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DQZ0WCq4.js","assets/index-BWqunxnW.js","assets/index-Dc-Ta0-n.css"])))=>i.map(i=>d[i]);
+import{a as e,t}from"./index-BWqunxnW.js";var n=e(`Share`,{web:()=>t(()=>import(`./web-DQZ0WCq4.js`).then(e=>new e.ShareWeb),__vite__mapDeps([0,1,2]))});export{n as Share};
+//# sourceMappingURL=esm-CA5dM4Z9.js.map
---
+++ pages-functions/dist/assets/esm-D6M4FYDP.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DUUfLePX.js","assets/index-9Yh9i-UM.js","assets/index-Dc-Ta0-n.css"])))=>i.map(i=>d[i]);
+import{a as e,t}from"./index-9Yh9i-UM.js";var n=e(`Share`,{web:()=>t(()=>import(`./web-DUUfLePX.js`).then(e=>new e.ShareWeb),__vite__mapDeps([0,1,2]))});export{n as Share};
+//# sourceMappingURL=esm-D6M4FYDP.js.map
